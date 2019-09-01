2ae3bb9801e7af17f691939a84ee6201d8bcb6c2
Jialin Li <32602256+JialinLiOSU@users.noreply.github.com>
2019-09-01 10:12:13 -0400

    update results of MLP forregion classification

--- Code Files/Designed architecture/MLP for map classification by region using Keras_traintestsplit4.py
+++ Code Files/Designed architecture/MLP for map classification by region using Keras_traintestsplit4.py
@@ -1,20 +1,22 @@
-# MLP using keras
+# MLP for world map classification using keras
 import numpy as np
 import keras
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Input
 from keras.layers import Dense, Flatten
 from keras.layers import Conv2D, MaxPooling2D
+from keras.utils.np_utils import to_categorical
 from PIL import Image
 import random
 import time
 import pickle
 
+
 # get the training data
 # path_source1='C:\\Users\\Administrator\\Desktop\\Dropbox\\Dissertation Materials\\Images for training\\NotMapsGrey\\'
 # path_source2='C:\\Users\\Administrator\\Desktop\\Dropbox\\Dissertation Materials\\Images for training\\MapsGrey\\'
-path_source1 = 'C:\\Users\\li.7957\\OneDrive\\Images for training\\map identification_world maps\\train\\'
-path_source2 = 'C:\\Users\\li.7957\\OneDrive\\Images for training\\map identification_world maps\\test\\'
+path_source1 = 'C:\\Users\\li.7957\\OneDrive\\Images for training\\region classification images for experiments\\train\\'
+path_source2 = 'C:\\Users\\li.7957\\OneDrive\\Images for training\\region classification images for experiments\\test\\'
 num_notmap = 500
 num_map = 500
 num_train = 700
@@ -27,13 +29,14 @@ height = 100
 num_pixels = width*height
 input_size = width*height*3
 input_shape = (width, height, 3)
-num_classes = 2
+num_classes = 4
 
 # point_generate_random(num_points,num_pixel)
-with open('C:\\Users\\li.7957\\OneDrive\\Images for training\\map identification_world maps\\test_identification_world_MLP.pickle', 'rb') as file:
+with open('C:\\Users\\li.7957\\OneDrive\\Images for training\\region classification images for experiments\\test_classification_regions_MLP.pickle', 'rb') as file:
     [x_test, y_test] = pickle.load(file)
-with open('C:\\Users\\li.7957\\OneDrive\\Images for training\\map identification_world maps\\train_identification_world_MLP.pickle', 'rb') as file:
+with open('C:\\Users\\li.7957\\OneDrive\\Images for training\\region classification images for experiments\\train_classification_regions_MLP.pickle', 'rb') as file:
     [x_train, y_train] = pickle.load(file)
+
 
 class AccuracyHistory(keras.callbacks.Callback):
     def on_train_begin(self, logs={}):
@@ -42,61 +45,54 @@ class AccuracyHistory(keras.callbacks.Callback):
     def on_epoch_end(self, batch, logs={}):
         self.acc.append(logs.get('acc'))
 
+
 history = AccuracyHistory()
 lr = 0.01
 beta_1 = 0.9
 beta_2 = 0.999
 
-str1 = "450 - 100 - 1" + "\n"
+str1 = " 1500 - 500 - 200 - 100 - 4" + "\n"
 
 for inx in range(1):
     model = Sequential()
-    model.add(Dense(450, input_dim=input_size, activation='relu'))
+    model.add(Dense(1500, input_dim=input_size, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(500, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(200, activation='relu'))
     model.add(Dropout(0.5))
     model.add(Dense(100, activation='relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(1, activation='sigmoid'))
+    model.add(Dense(4, activation='softmax'))
 
-    model.compile(loss=keras.losses.binary_crossentropy,
-                            optimizer=keras.optimizers.SGD(lr = lr),
-                            metrics=['accuracy'])
+    # sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 
-
-    # x_train=X_batches[0:num_train].reshape(num_train,input_size)
-    # x_test=X_batches[num_train:num_total].reshape(num_test,input_size)
-    # y_train=y_batches[0:num_train].reshape(num_train,1)
-    # y_test=y_batches[num_train:num_total].reshape(num_test,1)
-    # print('y_test:',y_test.reshape(1,num_test))
-
+    model.compile(loss = keras.losses.binary_crossentropy,
+              optimizer = keras.optimizers.SGD(lr = lr),
+              metrics=['accuracy'])
     batch_size = 5
     epochs = 100
 
-    # start=time.time() # start time for training
+    y_train = to_categorical(y_train, num_classes=4)
+    y_test = to_categorical(y_test, num_classes=4)
+
     model.fit(x_train, y_train,batch_size=batch_size,epochs=epochs,
                     verbose=2,validation_data=(x_test, y_test),
               callbacks=[history])
-
-    # end_train=time.time() # end time for training
-     # score = model.evaluate(x_test, y_test, batch_size=10)
-    score = model.evaluate(x_test, y_test, batch_size=batch_size, verbose=0)
+    score = model.evaluate(x_test, y_test, batch_size=batch_size)
     train_acc = history.acc[epochs - 1]
-    # end_test=time.time() # end time for testing
-    # train_time=end_train-start
-    # test_time=end_test-end_train
     test_loss=score[0]
     test_acc=score[1]
     print('Test loss:', test_loss)
     print('Test accuracy:', test_acc)
 
-
     str2 = 'Training accuracy:' + \
         str(train_acc) + ' Test accuracy:' + str(test_acc) + '\n'
-
-
-filename='Results_MLP_Identification'+'1'+'.txt'
+# y = model.predict(x_test)
+# print(y)
+# print(score)
+filename='Results_MLP_Region_Classification'+'1'+'.txt'
 file = open(filename,'a')
 file.write(str1) 
 file.write(str2)
 file.close() 
-
-
