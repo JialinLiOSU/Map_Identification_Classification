558fe2f8823ee4fecba35f9d8ef4268674dce1fd
Jialin Li <32602256+JialinLiOSU@users.noreply.github.com>
2020-06-08 17:26:25 -0400

    update CNN results

--- Code Files/Designed architecture/CNNs for world map classification by projection using 1250.py
+++ Code Files/Designed architecture/CNNs for world map classification by projection using 1250.py
@@ -16,7 +16,7 @@ import os
 import pickle
 
 # get the training data
-path_root = 'C:\\Users\\jiali\\OneDrive\\Images for training\\maps for classification of projections\\'
+path_root = 'C:\\Users\\li.7957\\OneDrive\\Images for training\\maps for classification of projections\\'
 # path_root = 'C:\\Users\\jiali\\OneDrive\\Images for training\\maps for classification of projections\\'
 path_source0 = path_root + 'Other_Projections_Maps\\'
 path_source1 = path_root+'Equirectangular_Projection_Maps\\'
