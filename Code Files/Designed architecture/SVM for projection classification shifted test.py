cea4bae7093bc9591f9b4e7b04e1eb55ea2b342c
Jialin Li <32602256+JialinLiOSU@users.noreply.github.com>
2020-06-16 22:24:05 -0400

    update shift test by CNN and SVM

--- Code Files/Designed architecture/SVM for projection classification shifted test.py
+++ Code Files/Designed architecture/SVM for projection classification shifted test.py
@@ -12,11 +12,12 @@ import time
 # get the training data
 path_root = 'C:\\Users\\li.7957\\OneDrive\\Images for training\\maps for classification of projections\\'
 # path_root = 'C:\\Users\\jiali\\OneDrive\\Images for training\\maps for classification of projections\\'
+path_source0 = path_root + 'Other_Projections_Maps\\'
 path_source1 = path_root+'Equirectangular_Projection_Maps\\'
 path_source2 = path_root+'Mercator_Projection_Maps\\'
 path_source3 = path_root+'EqualArea_Projection_Maps\\'
 path_source4 = path_root+'Robinson_Projection_Maps\\'
-path_source5 = path_root+'Horizontal rotated maps\\'
+path_source5 = path_root+'Horizontal rotated maps\\270\\'
 # img = Image.open('C:\\Users\\jiali\\OneDrive\\Images for training\\maps for classification of projections\\Equirectangular_Projection_Maps\\equirectangular_projection_map1.jpg')
 # path_source5='C:\\Users\\Administrator\\Desktop\\Dropbox\\Dissertation Materials\\Images for training\\NotMapsGrey\\'
 
@@ -30,8 +31,10 @@ num_pixels = width*height
 input_size = width*height*3
 
 data_pair = []
+num_classes = 5
 
 # Get the image data and store data into X_batches and y_batches
+OtherProjection_images = os.listdir(path_source0)
 Equirectangular_images = os.listdir(path_source1)
 Mercator_images = os.listdir(path_source2)
 EqualArea_images = os.listdir(path_source3)
@@ -40,8 +43,21 @@ Robinson_images = os.listdir(path_source4)
 rotatedImgList = []
 rotated_images = os.listdir(path_source5)
 
+# Read map images from other projections
 count = 0
 imgNameList = []
+for imgName in OtherProjection_images:
+    imgNameList.append(imgName)
+    fullName = path_source0 + imgName
+    img = Image.open(fullName)
+    img_resized = img.resize((width, height), Image.ANTIALIAS)
+    pixel_values = list(img_resized.getdata())
+    data_pair.append(pixel_values)
+    count = count + 1
+    if count >= 250:
+        break
+
+count = 0
 for imgName in Equirectangular_images:
     imgNameList.append(imgName)
     fullName = path_source1 + imgName
@@ -93,8 +109,8 @@ for rotatedImg in rotated_images:
     pixel_values = list(img_resized.getdata())
     rotatedImgList.append(pixel_values)
 
-num_total = num_maps_class*4
-train_size = 800
+num_total = num_maps_class*num_classes
+train_size = 1000
 
 data_pair_3 = []
 for i in range(num_total):
@@ -117,6 +133,9 @@ for i in range(num_total):
     elif i >= num_maps_class*3 and i < num_maps_class*4:
         # print(len(pixel_value_list))
         data_pair_3.append(pixel_value_list+[3])
+    elif i >= num_maps_class*4 and i < num_maps_class*5:
+        # print(len(pixel_value_list))
+        data_pair_3.append(pixel_value_list+[4])
 
 rotatedImgList_3 = []
 numRotatedImg = len(rotatedImgList)
@@ -129,7 +148,7 @@ for i in range(numRotatedImg):
         pixel_value_list.append(pixels[0])
         pixel_value_list.append(pixels[1])
         pixel_value_list.append(pixels[2])
-    rotatedImgList_3.append(pixel_value_list+[0])
+    rotatedImgList_3.append(pixel_value_list+[1])
 
 dp3_name = zip(data_pair_3,imgNameList)
 dp3_name = list(dp3_name)
@@ -139,7 +158,7 @@ len_x = len(data_pair_3[0])-1
 # random.seed(42)
 strList = []  # save the strings to be written in files
 
-for inx in range(1):
+for inx in range(3):
     print('Index of sets is: ', inx)
     strTemp = "sets of experiments" + str(inx)
     strList.append(strTemp)
@@ -177,7 +196,7 @@ for inx in range(1):
 
     y_train = y_train_array
     y_test = y_test_array
-    y_rotate = y_rotate_test_array
+    y_rotate_test = y_rotate_test_array
 
     x_train = [{j: x_train_array[i][j]
                 for j in range(input_size)} for i in range(train_size)]
@@ -199,39 +218,22 @@ for inx in range(1):
 
     prob = svm_problem(y_train, x_train)
 
-    # Part 1: Classification using linear SVMs
-    strTemp = "\nLinear kernel: "
+    # Part 3: Classification using polynomial SVM
+    strTemp = "\nPolynomial kernel: "
     strList.append(strTemp)
 
-    acc_c_list = []
-    start_train = time.time()  # start time for training
-    for c in c_list:
-        print('value of c is: ', c)
-        param = svm_parameter('-t 0 -v 5 -h 0 -c '+str(c))
-        m = svm_train(prob, param)
-        acc_c_list.append(m)
-    end_train = time.time()  # end time for training
-    train_time = end_train-start_train
-
-    index = np.argmax(acc_c_list)
-    index_c = index
-    # index_alpha=(index - 13*index_c)
-    c = c_list[index_c]
-    # alpha=alpha_list[index_alpha]
-    print('\n value of c is: ', c)
-    strTemp = ' value of c is:  ' + str(c)
-    strList.append(strTemp)
-
-    # print('value of alpha is: ',alpha)
-    param = svm_parameter('-t 0 -h 0 -c '+str(c))
+    c=2
+    alpha=0.0625
+    r=128
+    
+    param = svm_parameter('-t 1 -h 0 -g '+str(alpha)+' -c '+str(c)+' -r '+str(r))
     m = svm_train(prob, param)
     # column=index%13
-
     print('\nTraining acc:')
     p_label, p_acc, p_val = svm_predict(y_train, x_train, m)
     strTemp = ' Training acc:' + str(p_acc[0])
     strList.append(strTemp)
-    
+        
     print(' Testing acc:')
     start_test = time.time()
     p_label, p_acc, p_val = svm_predict(y_test, x_test, m)
@@ -240,100 +242,20 @@ for inx in range(1):
     strTemp = ' Testing acc:' + str(p_acc[0])
     strList.append(strTemp)
 
-    p_label, p_acc, p_val = svm_predict(y_rotate, x_rotate_test, m)
-
-    ####     calculate Precise, Recall and F1 score      #####
-    # p_label is the predicted class labels
-    # y_test is the desired class labels
-
-    # number of predicted label
-    count_p_label0 = p_label.count(0.0)
-    count_p_label1 = p_label.count(1.0)
-    count_p_label2 = p_label.count(2.0)
-    count_p_label3 = p_label.count(3.0)
-    # number of desired label
-    count_d_label0 = y_test.count(0)
-    count_d_label1 = y_test.count(1)
-    count_d_label2 = y_test.count(2)
-    count_d_label3 = y_test.count(3)
-    # number of real label
-    count_r_label0 = 0
-    count_r_label1 = 0
-    count_r_label2 = 0
-    count_r_label3 = 0
-
-    # collect wrongly classified images
-    incorrectImgNameStrList = []
-    for i in range(len(p_label)):
-        if p_label[i] == 0 and y_test[i] == 0:
-            count_r_label0 = count_r_label0 + 1
-        elif p_label[i] == 1 and y_test[i] == 1:
-            count_r_label1 = count_r_label1 + 1
-        elif p_label[i] == 2 and y_test[i] == 2:
-            count_r_label2 = count_r_label2 + 1
-        elif p_label[i] == 3 and y_test[i] == 3:
-            count_r_label3 = count_r_label3 + 1
-        else:
-            imgName = imgNameList[i + train_size]
-            incorrectImgString = '\n' + imgName + ',' + str(y_test[i]) + ',' + str(p_label[i])
-            incorrectImgNameStrList.append(incorrectImgString)
-    
-    # precise for the four classes
-    precise = []
-    precise.append(count_r_label0/count_p_label0)
-    precise.append(count_r_label1/count_p_label1)
-    precise.append(count_r_label2/count_p_label2)
-    precise.append(count_r_label3/count_p_label3)
-    
-    strTemp = " Precise:"
-    strList.append(strTemp)
-    strTemp = ' '
-    for p in precise:
-        strTemp = strTemp + str(p)+','
-    strList.append(strTemp)
-    
-
-    # recall for the four classes
-    recall = []
-    recall.append(count_r_label0 / count_d_label0)
-    recall.append(count_r_label1 / count_d_label1)
-    recall.append(count_r_label2 / count_d_label2)
-    recall.append(count_r_label3 / count_d_label3)
-
-    strTemp = " Recall:"
-    strList.append(strTemp)
-    strTemp = ' '
-    for r in recall:
-        strTemp = strTemp + str(r)+','
-    strList.append(strTemp)
-    
-
-    # recall for the four classes   
-    F1score = []
-    F1score.append(2/((1/precise[0])+(1/recall[0])))
-    F1score.append(2/((1/precise[1])+(1/recall[1])))
-    F1score.append(2/((1/precise[2])+(1/recall[2])))
-    F1score.append(2/((1/precise[3])+(1/recall[3])))
-
-    strTemp = " F1 Score:"
-    strList.append(strTemp)
-    strTemp = ''
-    for f1 in F1score:
-        strTemp = strTemp + str(f1)+','
+    print(' Testing acc:')
+    start_test = time.time()
+    p_label, p_acc, p_val = svm_predict(y_rotate_test, x_rotate_test, m)
+    end_test = time.time()
+    test_time = end_test - start_test
+    strTemp = '\nShifted Testing acc:' + str(p_acc[0])
     strList.append(strTemp)
 
-    print("train_time:" + str(train_time)+"\n")
-    print("test_time:" + str(test_time) + "\n")
-    strTemp = " train_time:" + str(train_time)
-    strList.append(strTemp)
-    strTemp = " test_time:" + str(test_time)
-    strList.append(strTemp)
-
-filename = 'SVMforProjection1'+'.txt'
-file = open(filename, 'a')
+filename='SVM_ShiftedTest'+'.txt'
+file = open(filename,'a')
 file.writelines(strList)
-file.writelines(incorrectImgNameStrList)
-file.close()
+file.close() 
+
+    
 
 # alpha=64
 # c=64
