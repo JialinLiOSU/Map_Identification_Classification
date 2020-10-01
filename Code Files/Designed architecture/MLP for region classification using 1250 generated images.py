74d227d54b19a8c3ac0d92eb756dfc3998059a8e
Jialin Li <32602256+JialinLiOSU@users.noreply.github.com>
2020-10-01 08:42:14 -0400

    update MLP results

--- Code Files/Designed architecture/MLP for region classification using 1250 generated images.py
+++ Code Files/Designed architecture/MLP for region classification using 1250 generated images.py
@@ -148,16 +148,16 @@ test_loss_list=[]
 test_acc_list=[]
 
 # layerSettings = [[1000,500,200,100]]
-layerSettings = [[100],[150],[200],[300],[350],[400],[450],[500]]
+# layerSettings = [[100],[150],[200],[300],[350],[400],[450],[500]]
 # layerSettings = [[150,100],[200,100],[250,100],[300,100],[400,100],[450,100],[500,100]]
-# layerSettings = [[200,200,100],[300,200,100],[400,200,100],[500,200,100],[600,200,100]]
+layerSettings = [[200,200,100],[300,200,100],[400,200,100],[500,200,100],[600,200,100]]
 # layerSettings = [[400]]
 for ls in layerSettings:
     strList = []  # save the strings to be written in files
     incorrectImgNameStrList = []   
 
-    strTemp = "\n"+str(ls[0])  + "-5"
-    # strTemp = "\n"+str(ls[0]) + "-"+str(ls[1]) + "-"+str(ls[2]) + "-5"
+    # strTemp = "\n"+str(ls[0])  + "-5"
+    strTemp = "\n"+str(ls[0]) + "-"+str(ls[1]) + "-"+str(ls[2]) + "-5"
     # strTemp = "\n"+str(ls[0]) + "-"+str(ls[1]) + "-"+str(ls[2]) + "-"+str(ls[3]) 
     strList.append(strTemp)
 
@@ -169,10 +169,10 @@ for ls in layerSettings:
         model = Sequential()
         model.add(Dense(ls[0], input_dim=input_size, activation='relu'))
         model.add(Dropout(0.5))
-        # model.add(Dense(ls[1], activation='relu'))
-        # model.add(Dropout(0.5))
-        # model.add(Dense(ls[2], activation='relu'))
-        # model.add(Dropout(0.5))
+        model.add(Dense(ls[1], activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(ls[2], activation='relu'))
+        model.add(Dropout(0.5))
         # model.add(Dense(ls[3], activation='relu'))
         # model.add(Dropout(0.5))
         model.add(Dense(num_classes, activation='softmax'))
